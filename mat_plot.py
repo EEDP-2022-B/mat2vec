4a44830cea55ad2e3d1b02cf00b0d9bce38b6b5b
Jeffrey Maskalunas <99277283+JeffreyMaskalunas@users.noreply.github.com>
2022-02-15 08:00:59 -0500

    Updating plot axes and title

--- mat_plot.py
+++ mat_plot.py
@@ -4,8 +4,6 @@
 from gensim.models import Word2Vec
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-import pickle
-import numpy as np
 
 w2v_model = Word2Vec.load(r"mat2vec/training/models/pretrained_embeddings")
 words = list(w2v_model.wv.vocab)
@@ -18,6 +16,9 @@ result = pca.fit_transform(X)
 
 plt.scatter(result[:, 0], result[:, 1])
 words = list(w2v_model.wv.vocab)
+plt.xlabel("First Principal Component")
+plt.ylabel("Second Principal Component")
+plt.title("Scatter Plot of the Word Embeddings")
 # Create a scatter plot of the projection of the word embeddings
 plt.show()
 
