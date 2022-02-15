e5ccf0bbc936c24c40db94a2a280ea13f5aa5a87
Jeffrey Maskalunas <99277283+JeffreyMaskalunas@users.noreply.github.com>
2022-02-15 07:18:25 -0500

    Add files via upload

---
+++ mat_plot.py
@@ -0,0 +1,23 @@
+# Scatter plot to visualize word embeddings using PCA
+# Reference: https://builtin.com/machine-learning/nlp-word2vec-python
+
+from gensim.models import Word2Vec
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+
+w2v_model = Word2Vec.load(r"mat2vec/training/models/pretrained_embeddings")
+words = list(w2v_model.wv.vocab)
+# Display the vocabulary words in the console
+print(words)
+
+X = w2v_model[w2v_model.wv.vocab]
+pca = PCA(n_components=2)
+result = pca.fit_transform(X)
+
+plt.scatter(result[:, 0], result[:, 1])
+words = list(w2v_model.wv.vocab)
+# Create a scatter plot of the projection of the word embeddings
+plt.show()
+
